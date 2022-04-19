fea2f6d84a9459a813db150b7efde96b2463d9d9
Esther Bauzou <48768205+PetitChampi@users.noreply.github.com>
2022-04-19 16:50:44 +0200

    handling file changes

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -13,8 +13,8 @@
       v-model="description"
     ></textarea>
     <label>Upload playlist cover image</label>
-    <input type="file">
-    <div class="error"></div>
+    <input type="file" @change="handleChange">
+    <div class="error">{{ fileError }}</div>
     <button>Create</button>
   </form>
 </template>
@@ -26,12 +26,31 @@ export default {
   setup() {
     const title = ref('')
     const description = ref('')
+    const file = ref(null)
+    const fileError = ref(null)
 
     const handleSubmit = () => {
-      console.log(title.value, description.value)
+      if (file.value) {
+        console.log(title.value, description.value, file.value)
+      }
     }
 
-    return { title, description, handleSubmit }
+    // allowed file types
+    const types = ['image/png', 'image/jpeg']
+
+    const handleChange = (e) => {
+      const selected = e.target.files[0]
+      console.log(selected)
+      if (selected && types.includes(selected.type)) {
+        file.value = selected
+        fileError.value = null
+      } else {
+        file.value = null
+        fileError.value = 'Please select an image file (png or jpg)'
+      }
+    }
+
+    return { title, description, handleSubmit, handleChange, fileError }
   }
 }
 </script>
