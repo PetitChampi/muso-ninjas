a3fd3b2c65c7a392310fa3590d4428432504e2a5
Esther Bauzou <48768205+PetitChampi@users.noreply.github.com>
2022-04-19 11:35:37 +0200

    init firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqMBRANhqa1CShVVGuXYeePkSfjuhfEUA",
+  authDomain: "muso-ninjas-3c3c5.firebaseapp.com",
+  projectId: "muso-ninjas-3c3c5",
+  storageBucket: "muso-ninjas-3c3c5.appspot.com",
+  messagingSenderId: "365478837755",
+  appId: "1:365478837755:web:e0a30e25cbcd914e675f5e"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
